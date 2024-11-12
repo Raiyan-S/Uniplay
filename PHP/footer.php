20967caf8478d024ff353c98dfbb7199b9d10ca8
Raiyan.S <85458945+Raiyan-S@users.noreply.github.com>
2024-11-12 19:15:12 +0300

    footer fix

--- PHP/footer.php
+++ PHP/footer.php
@@ -2,11 +2,11 @@
 Name: Raiyan Subedar -->
 
 <!-- PHP/footer.php -->
-<footer>
+<<div class="footer">>
     <div class="footer-left">
         <p>&copy; 2024 Uniplay. All rights reserved.</p>
     </div>
     <div class="footer-right">
         <p>Al-Hamra'a, Jeddah, Saudi Arabia</p>
     </div>
-</footer>
+</div>
--- PHP/home.php
+++ PHP/home.php
@@ -1,7 +1,6 @@
 <!-- ID: 2136108 
 Name: Raiyan Subedar -->
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
 <!-- home.php -->
@@ -10,104 +9,103 @@ $title = 'Uniplay - Home Page';
 include 'header.php';
 ?>
 
-    <!-- Main Content -->
-    <div class="main-content">
-        <div class="banner-row">
-            <div class="banner-info">
-                <h1>Everything Everywhere All at Once</h1>
-                <p>Action, Drama</p>
-            </div>
-            <button class="details-btn">Details</button>
+<!-- Main Content -->
+<div class="main-content">
+    <div class="banner-row">
+        <div class="banner-info">
+            <h1>Everything Everywhere All at Once</h1>
+            <p>Action, Drama</p>
         </div>
-        <!-- First Row: Movie/Show Posters -->
-        <div class="home-first-row">
-            <div class="top-week-container">
-                <h2 class="trending-header">Trending Now</h2>
-                <button class="more-button">More</button>
-            </div>
-            <div class="posters-container">
-                <div class="poster">
-                    <a href="movie.php">
-                        <img src="../Pictures/TheDarkKnight.jpg" alt="The Dark Knight" class="poster" />
-                    </a>
-                    <div class="info">
-                        <h3>The Dark Knight</h3>
-                        <p>Action, Thriller</p>
-                    </div>
-                </div>
-                <div class="poster" style="background-image: url('../Pictures/TheGodFather.jpg');">
-                    <div class="info">
-                        <h3>The Godfather</h3>
-                        <p>Drama, Crime</p>
-                    </div>
-                </div>
-                <div class="poster" style="background-image: url('../Pictures/GravityFalls.jpg');">
-                    <div class="info">
-                        <h3>Gravity Falls</h3>
-                        <p>Animation, Comedy</p>
-                    </div>
-                </div>
-                <div class="poster" style="background-image: url('../Pictures/WhenTheySeeUs.jpg');">
-                    <div class="info">
-                        <h3>When They See Us</h3>
-                        <p>Drama, History</p>
-                    </div>
-                </div>
-                <div class="poster" style="background-image: url('../Pictures/SpidermanAcrossTheSpiderverse.jpg');">
-                    <div class="info">
-                        <h3>Spider-Man: Across the Spider-Verse</h3>
-                        <p>Animation, Adventure</p>
-                    </div>
-                </div>
-                <div class="poster" style="background-image: url('../Pictures/BreakingBad.jpg');">
-                    <div class="info">
-                        <h3>Breaking Bad</h3>
-                        <p>Crime, Thriller</p>
-                    </div>
-                </div>
-            </div>
+        <button class="details-btn">Details</button>
+    </div>
+    <!-- First Row: Movie/Show Posters -->
+    <div class="home-first-row">
+        <div class="top-week-container">
+            <h2 class="trending-header">Trending Now</h2>
+            <button class="more-button">More</button>
         </div>
-
-        <!-- Second Row: Top in a Week -->
-        <div class="top-week-section">
-            <div class="top-week-container">
-                <h2 class="top-week-header">Top in a Week</h2>
-                <button class="more-button">More</button>
+        <div class="posters-container">
+            <div class="poster">
+                <a href="movie.php">
+                    <img src="../Pictures/TheDarkKnight.jpg" alt="The Dark Knight" class="poster" />
+                </a>
+                <div class="info">
+                    <h3>The Dark Knight</h3>
+                    <p>Action, Thriller</p>
+                </div>
             </div>
-            <div class="posters-row">
-                <div class="poster" style="background-image: url('../Pictures/BluePlanetII.jpg');">
-                    <div class="info">
-                        <h3>Blue Planet II</h3>
-                        <p>Documentary, Nature</p>
-                    </div>
+            <div class="poster" style="background-image: url('../Pictures/TheGodFather.jpg');">
+                <div class="info">
+                    <h3>The Godfather</h3>
+                    <p>Drama, Crime</p>
                 </div>
-                <div class="poster" style="background-image: url('../Pictures/FullMetalAlchemistBrotherhood.jpg');">
-                    <div class="info">
-                        <h3>Fullmetal Alchemist: Brotherhood</h3>
-                        <p>Anime, Fantasy</p>
-                    </div>
+            </div>
+            <div class="poster" style="background-image: url('../Pictures/GravityFalls.jpg');">
+                <div class="info">
+                    <h3>Gravity Falls</h3>
+                    <p>Animation, Comedy</p>
                 </div>
-                <div class="poster" style="background-image: url('../Pictures/DeadpoolANDWolverine.jpg');">
-                    <div class="info">
-                        <h3>Deadpool &amp; Wolverine</h3>
-                        <p>Action, Comedy</p>
-                    </div>
+            </div>
+            <div class="poster" style="background-image: url('../Pictures/WhenTheySeeUs.jpg');">
+                <div class="info">
+                    <h3>When They See Us</h3>
+                    <p>Drama, History</p>
                 </div>
-                <div class="poster" style="background-image: url('../Pictures/TheOffice.jpg');">
-                    <div class="info">
-                        <h3>The Office</h3>
-                        <p>Sitcom, Comedy</p>
-                    </div>
+            </div>
+            <div class="poster" style="background-image: url('../Pictures/SpidermanAcrossTheSpiderverse.jpg');">
+                <div class="info">
+                    <h3>Spider-Man: Across the Spider-Verse</h3>
+                    <p>Animation, Adventure</p>
+                </div>
+            </div>
+            <div class="poster" style="background-image: url('../Pictures/BreakingBad.jpg');">
+                <div class="info">
+                    <h3>Breaking Bad</h3>
+                    <p>Crime, Thriller</p>
                 </div>
             </div>
         </div>
-
     </div>
 
-    <!-- Footer Section -->
-    <div class="footer">
-        <?php include 'footer.php'; ?>
+    <!-- Second Row: Top in a Week -->
+    <div class="top-week-section">
+        <div class="top-week-container">
+            <h2 class="top-week-header">Top in a Week</h2>
+            <button class="more-button">More</button>
+        </div>
+        <div class="posters-row">
+            <div class="poster" style="background-image: url('../Pictures/BluePlanetII.jpg');">
+                <div class="info">
+                    <h3>Blue Planet II</h3>
+                    <p>Documentary, Nature</p>
+                </div>
+            </div>
+            <div class="poster" style="background-image: url('../Pictures/FullMetalAlchemistBrotherhood.jpg');">
+                <div class="info">
+                    <h3>Fullmetal Alchemist: Brotherhood</h3>
+                    <p>Anime, Fantasy</p>
+                </div>
+            </div>
+            <div class="poster" style="background-image: url('../Pictures/DeadpoolANDWolverine.jpg');">
+                <div class="info">
+                    <h3>Deadpool &amp; Wolverine</h3>
+                    <p>Action, Comedy</p>
+                </div>
+            </div>
+            <div class="poster" style="background-image: url('../Pictures/TheOffice.jpg');">
+                <div class="info">
+                    <h3>The Office</h3>
+                    <p>Sitcom, Comedy</p>
+                </div>
+            </div>
+        </div>
     </div>
+
+</div>
+
+<!-- Footer Section -->
+<?php include 'footer.php'; ?>
+
 </body>
 
 </html>
