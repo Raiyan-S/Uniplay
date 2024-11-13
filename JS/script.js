0657956b43bcec556aa48a95dc16c1dc2cffccae
Raiyan.S <85458945+Raiyan-S@users.noreply.github.com>
2024-11-13 12:51:21 +0300

    picture update

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,8 @@
-function showImage(src) {
+function showImage(src, caption) {
     var largeImage = document.getElementById("large-image");
+    var imageCaption = document.getElementById("image-caption");
     largeImage.src = src;
     largeImage.style.display = "block";
+    imageCaption.textContent = caption;
+    imageCaption.style.display = "block";
 }
