9978185b705521db21cb16a51827e935faf58447
Raiyan.S <85458945+Raiyan-S@users.noreply.github.com>
2024-11-13 14:29:27 +0300

    Update footer.php

--- PHP/footer.php
+++ PHP/footer.php
@@ -11,16 +11,13 @@ Name: Raiyan Subedar -->
     </div>
     <div class="footer-icons">
         <p>
-            <a href="https://jigsaw.w3.org/css-validator/check/referer">
-                <img style="border:0;width:88px;height:31px"
-                    src="https://jigsaw.w3.org/css-validator/images/vcss"
-                    alt="Valid CSS!" />
-            </a>
+            <a href="https://validator.w3.org/markup/check?uri=referer"><img
+                    src="https://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31"
+                    width="88" /></a>
         </p>
         <p>
             <a href="https://jigsaw.w3.org/css-validator/check/referer">
-                <img style="border:0;width:88px;height:31px"
-                    src="https://jigsaw.w3.org/css-validator/images/vcss-blue"
+                <img style="border:0;width:88px;height:31px" src="https://jigsaw.w3.org/css-validator/images/vcss-blue"
                     alt="Valid CSS!" />
             </a>
         </p>
