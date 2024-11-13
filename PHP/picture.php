f8d3934142f82e3a8fd3a9008dccc4fc777908ac
Raiyan.S <85458945+Raiyan-S@users.noreply.github.com>
2024-11-13 12:56:46 +0300

    picture update

--- PHP/picture.php
+++ PHP/picture.php
@@ -12,36 +12,23 @@ include 'header.php';
 <!-- Picture Gallery Section -->
 <div class="gallery">
     <h1>Picture Gallery</h1>
+    <div class="thumbnails">
+        <img src="../Pictures/TheDarkKnight.jpg" alt="The Dark Knight" class="thumbnail" onclick="showImage('../Pictures/TheDarkKnight.jpg', 'The Dark Knight')" />
+        <img src="../Pictures/TheGodFather.jpg" alt="The Godfather" class="thumbnail" onclick="showImage('../Pictures/TheGodFather.jpg', 'The Godfather')" />
+        <img src="../Pictures/GravityFalls.jpg" alt="Gravity Falls" class="thumbnail" onclick="showImage('../Pictures/GravityFalls.jpg', 'Gravity Falls')" />
+        <img src="../Pictures/WhenTheySeeUs.jpg" alt="When They See Us" class="thumbnail" onclick="showImage('../Pictures/WhenTheySeeUs.jpg', 'When They See Us')" />
+        <img src="../Pictures/SpidermanAcrossTheSpiderverse.jpg" alt="Spider-Man: Across the Spider-Verse" class="thumbnail" onclick="showImage('../Pictures/SpidermanAcrossTheSpiderverse.jpg', 'Spider-Man: Across the Spider-Verse')" />
+        <img src="../Pictures/BreakingBad.jpg" alt="Breaking Bad" class="thumbnail" onclick="showImage('../Pictures/BreakingBad.jpg', 'Breaking Bad')" />
+        <img src="../Pictures/BluePlanetII.jpg" alt="Blue Planet II" class="thumbnail" onclick="showImage('../Pictures/BluePlanetII.jpg', 'Blue Planet II')" />
+        <img src="../Pictures/FullMetalAlchemistBrotherhood.jpg" alt="Fullmetal Alchemist: Brotherhood" class="thumbnail" onclick="showImage('../Pictures/FullMetalAlchemistBrotherhood.jpg', 'Fullmetal Alchemist: Brotherhood')" />
+        <img src="../Pictures/DeadpoolANDWolverine.jpg" alt="Deadpool & Wolverine" class="thumbnail" onclick="showImage('../Pictures/DeadpoolANDWolverine.jpg', 'Deadpool & Wolverine')" />
+        <img src="../Pictures/TheOffice.jpg" alt="The Office" class="thumbnail" onclick="showImage('../Pictures/TheOffice.jpg', 'The Office')" />
+    </div>
+
     <div class="large-image">
         <img id="large-image" src="" alt="Large Image" style="display:none;" />
         <p id="image-caption" style="display:none; color: #ffffff; margin-top: 10px;"></p>
     </div>
-    <div class="thumbnails">
-        <img src="../Pictures/TheDarkKnight.jpg" alt="The Dark Knight" class="thumbnail"
-            onclick="showImage('../Pictures/TheDarkKnight.jpg', 'The Dark Knight')" />
-        <img src="../Pictures/TheGodFather.jpg" alt="The Godfather" class="thumbnail"
-            onclick="showImage('../Pictures/TheGodFather.jpg', 'The Godfather')" />
-        <img src="../Pictures/GravityFalls.jpg" alt="Gravity Falls" class="thumbnail"
-            onclick="showImage('../Pictures/GravityFalls.jpg', 'Gravity Falls')" />
-        <img src="../Pictures/WhenTheySeeUs.jpg" alt="When They See Us" class="thumbnail"
-            onclick="showImage('../Pictures/WhenTheySeeUs.jpg', 'When They See Us')" />
-        <img src="../Pictures/SpidermanAcrossTheSpiderverse.jpg" alt="Spider-Man: Across the Spider-Verse"
-            class="thumbnail"
-            onclick="showImage('../Pictures/SpidermanAcrossTheSpiderverse.jpg', 'Spider-Man: Across the Spider-Verse')" />
-        <img src="../Pictures/BreakingBad.jpg" alt="Breaking Bad" class="thumbnail"
-            onclick="showImage('../Pictures/BreakingBad.jpg', 'Breaking Bad')" />
-        <img src="../Pictures/BluePlanetII.jpg" alt="Blue Planet II" class="thumbnail"
-            onclick="showImage('../Pictures/BluePlanetII.jpg', 'Blue Planet II')" />
-        <img src="../Pictures/FullMetalAlchemistBrotherhood.jpg" alt="Fullmetal Alchemist: Brotherhood"
-            class="thumbnail"
-            onclick="showImage('../Pictures/FullMetalAlchemistBrotherhood.jpg', 'Fullmetal Alchemist: Brotherhood')" />
-        <img src="../Pictures/DeadpoolANDWolverine.jpg" alt="Deadpool & Wolverine" class="thumbnail"
-            onclick="showImage('../Pictures/DeadpoolANDWolverine.jpg', 'Deadpool & Wolverine')" />
-        <img src="../Pictures/TheOffice.jpg" alt="The Office" class="thumbnail"
-            onclick="showImage('../Pictures/TheOffice.jpg', 'The Office')" />
-    </div>
-
-
 </div>
 
 <!-- JavaScript for Picture Gallery -->
@@ -50,5 +37,4 @@ include 'header.php';
 <!-- Footer Section -->
 <?php include 'footer.php'; ?>
 </body>
-
 </html>
