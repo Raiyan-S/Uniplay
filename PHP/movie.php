0107b08e76696e1a9dd32de54829cdf7c6905122
Raiyan.S <85458945+Raiyan-S@users.noreply.github.com>
2024-11-14 19:12:43 +0300

    movie video update

--- PHP/movie.php
+++ PHP/movie.php
@@ -20,15 +20,15 @@ include 'header.php';
     <div class="movie-details">
         <div class="poster" style="background-image: url('../Pictures/TheDarkKnight.jpg');"></div>
         <div class="movie-info">
-            <h2>The Dark Knight</h2>
+            <h1>The Dark Knight</h1>
             <p>Rating: &#9733;&#9733;&#9733;&#9733;&#9734;</p>
             <div class="description-box">
-                <h3>Description</h3>
+                <h2>Description</h2>
                 <p>When a menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman, James
                     Gordon and Harvey Dent must work together to put an end to the madness.</p>
             </div>
             <div class="watch-box">
-                <h3>Where to Watch</h3>
+                <h2>Where to Watch</h2>
                 <p>Streaming on: Netflix, Max, Hulu</p>
             </div>
             <button class="favorite-button">Add to Favorites</button>
@@ -39,12 +39,11 @@ include 'header.php';
         </div>
         <div class="video-trailer-container">
             <div class="video-trailer">
-                <h4>Trailer</h4>
-                <object data="https://www.youtube.com/embed/EXeTwQWrcwY" type="text/html" width="640" height="360">
-                    <param name="movie" value="https://www.youtube.com/embed/EXeTwQWrcwY" />
-                    <param name="allowFullScreen" value="true" />
-                    <param name="frameborder" value="0" />
-                    <embed src="https://www.youtube.com/embed/EXeTwQWrcwY" type="application/x-shockwave-flash" width="640" height="360" allowfullscreen="true" frameborder="0"></embed>
+                <h3>Trailer</h3>
+                <object data="https://www.youtube.com/embed/wxN1T1uxQ2g" type="text/html">
+                    <p>Your browser does not support embedded videos. You can watch the trailer on
+                        <a href="https://www.youtube.com/watch?v=wxN1T1uxQ2g">YouTube</a>.
+                    </p>
                 </object>
             </div>
         </div>
