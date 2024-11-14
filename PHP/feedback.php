84031cb080c1ac3686fd5e5c919f2173d8d6ef50
Raiyan.S <85458945+Raiyan-S@users.noreply.github.com>
2024-11-14 12:49:02 +0300

    feedback test update

--- PHP/feedback.php
+++ PHP/feedback.php
@@ -11,69 +11,67 @@ include 'header.php';
 
 <!-- Feedback Form Section -->
 <div class="feedback-wrapper">
-    <h2>Feedback Form</h2>
-    <div class="feedback-container">
-        <form action="submit_feedback.php" method="post">
-            <!-- Personal Information Section -->
-            <fieldset>
-                <legend>Personal Information</legend>
-                <div>
-                    <label for="name">Name:</label>
-                    <input type="text" id="name" name="name" />
-                </div>
-                <div>
-                    <label for="email">Email:</label>
-                    <input type="text" id="email" name="email" />
-                </div>
-                <div>
-                    <label for="phone">Phone:</label>
-                    <input type="text" id="phone" name="phone" />
-                </div>
-                <div>
-                    <label for="age">Age:</label>
-                    <input type="text" id="age" name="age" />
-                </div>
-            </fieldset>
-
-            <!-- Feedback Section -->
-            <fieldset>
-                <legend>Your Feedback</legend>
-                <div>
-                    <label for="rating">Rate Our Service:</label>
-                    <select id="rating" name="rating">
-                        <option value="excellent">Excellent</option>
-                        <option value="good">Good</option>
-                        <option value="average">Average</option>
-                        <option value="poor">Poor</option>
-                    </select>
-                </div>
-                <div>
-                    <label>Would you recommend us?</label>
-                    <input type="radio" id="recommend_yes" name="recommend" value="yes" />
-                    <label for="recommend_yes">Yes</label>
-                    <input type="radio" id="recommend_no" name="recommend" value="no" />
-                    <label for="recommend_no">No</label>
-                </div>
-                <div>
-                    <label>Services Used:</label>
-                    <input type="checkbox" id="service_movies" name="services" value="movies" />
-                    <label for="service_movies">Movies</label>
-                    <input type="checkbox" id="service_series" name="services" value="series" />
-                    <label for="service_series">Series</label>
-                </div>
-                <div>
-                    <label for="feedback">Additional Feedback:</label>
-                    <br>
-                    <textarea id="feedback" name="feedback" rows="4" cols="50"></textarea>
-                </div>
-            </fieldset>
-            <br>
-            <!-- Submit Button -->
+    <form action="submit_feedback.php" method="post">
+        <h1>Feedback Form</h1>
+        <!-- Personal Information Section -->
+        <fieldset>
+            <legend>Personal Information</legend>
             <div>
-                <input type="submit" value="Submit Feedback" />
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="name" />
             </div>
-        </form>
-    </div>
+            <div>
+                <label for="email">Email:</label>
+                <input type="text" id="email" name="email" />
+            </div>
+            <div>
+                <label for="phone">Phone:</label>
+                <input type="text" id="phone" name="phone" />
+            </div>
+            <div>
+                <label for="age">Age:</label>
+                <input type="text" id="age" name="age" />
+            </div>
+        </fieldset>
+
+        <!-- Feedback Section -->
+        <fieldset>
+            <legend>Your Feedback</legend>
+            <div>
+                <label for="rating">Rate Our Service:</label>
+                <select id="rating" name="rating">
+                    <option value="excellent">Excellent</option>
+                    <option value="good">Good</option>
+                    <option value="average">Average</option>
+                    <option value="poor">Poor</option>
+                </select>
+            </div>
+            <div>
+                <label>Would you recommend us?</label>
+                <input type="radio" id="recommend_yes" name="recommend" value="yes" />
+                <label for="recommend_yes">Yes</label>
+                <input type="radio" id="recommend_no" name="recommend" value="no" />
+                <label for="recommend_no">No</label>
+            </div>
+            <div>
+                <label>Services Used:</label>
+                <input type="checkbox" id="service_movies" name="services" value="movies" />
+                <label for="service_movies">Movies</label>
+                <input type="checkbox" id="service_series" name="services" value="series" />
+                <label for="service_series">Series</label>
+            </div>
+            <div>
+                <label for="feedback">Additional Feedback:</label>
+                <br>
+                <textarea id="feedback" name="feedback" rows="4" cols="50"></textarea>
+            </div>
+        </fieldset>
+        <br>
+        <!-- Submit Button -->
+        <div>
+            <input type="submit" value="Submit Feedback" />
+        </div>
+    </form>
     <div class="image-container">
         <img src="../Pictures/Feedback.png" alt="Feedback Image" class="feedback-image" />
     </div>
