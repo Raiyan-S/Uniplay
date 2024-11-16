2691ac10b911b158e2eaf60e1b40901b7a1d2f8a
Raiyan.S <85458945+Raiyan-S@users.noreply.github.com>
2024-11-17 01:37:21 +0300

    Update footer.php

--- Includes/footer.php
+++ Includes/footer.php
@@ -10,10 +10,11 @@ Name: Raiyan Subedar -->
         <p>Al-Hamra'a, Jeddah, Saudi Arabia</p>
     </div>
     <div class="footer-icons">
-    <p>
-    <a href="https://validator.w3.org/markup/check?uri=referer"><img
-      src="https://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Transitional" height="31" width="88" /></a>
-  </p>
+        <p>
+            <a href="https://validator.w3.org/check?uri=uniplay-production.up.railway.app"><img
+                    src="https://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31"
+                    width="88" /></a>
+        </p>
         <p>
             <a href="https://jigsaw.w3.org/css-validator/check/referer">
                 <img style="border:0;width:88px;height:31px" src="https://jigsaw.w3.org/css-validator/images/vcss-blue"
