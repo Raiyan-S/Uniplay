985489744329119d9fd06e25c656f41b870f16a9
Raiyan.S <85458945+Raiyan-S@users.noreply.github.com>
2024-12-04 10:57:03 +0300

    Update submit_feedback.php

--- PHP/submit_feedback.php
+++ PHP/submit_feedback.php
@@ -77,14 +77,11 @@ mysqli_close($conn);
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-    <title>Submit Feedback</title>
-    <link rel="stylesheet" href="../CSS/style.css" type="text/css" />
-</head>
+<<?php
+$title = 'Uniplay - Feedback';
+include '../Includes/header.php';
+?>
 <body>
-    <div>
-        <?php include '../Includes/header.php'; ?>
-    </div>
     <div class="feedback-wrapper">
         <div class="feedback-container">
             <h1>Submit Feedback</h1>
